c44ea37eeac0540d89a8bbf9a2360c8acf56fe57
Foster Brereton <fbrereto@adobe.com>
2017-06-15 09:32:43 -0700

    working on build script

--- configure.sh
+++ configure.sh
@@ -2,8 +2,7 @@
 
 # Run a command, and echo before doing so. Also checks the exit
 # status and quits if there was an error.
-echo_run ()
-{
+echo_run () {
     echo "EXEC : $@"
     "$@"
     r=$?
@@ -12,47 +11,55 @@ echo_run ()
     fi
 }
 
+git_dependency() {
+    local URL=$1
+    local DIR=$2
+    local BRANCH=$3
+
+    if [ -z "$BRANCH" ]; then
+        BRANCH=$TRAVIS_BRANCH
+    fi
+
+    if [ ! -e "$DIR" ]; then
+        echo "INFO : Cloning $DIR..."
+        echo_run git clone --branch=$BRANCH $URL $DIR
+    else
+        echo "INFO : Pulling $DIR..."
+
+        echo_run cd $DIR
+        echo_run git branch -u origin/$BRANCH
+        echo_run git checkout $BRANCH
+        echo_run git pull origin $BRANCH
+        echo_run cd ..
+    fi
+
+    return 0
+}
+
 echo_run cd `dirname $0`
 
 BOOST_MAJOR=1
-BOOST_MINOR=54
+BOOST_MINOR=64
 BOOST_DOT=0
 BOOST_DT_VER=${BOOST_MAJOR}.${BOOST_MINOR}.${BOOST_DOT}
 BOOST_US_VER=${BOOST_MAJOR}_${BOOST_MINOR}_${BOOST_DOT}
 BOOST_DIR=boost_${BOOST_US_VER}
 BOOST_TAR=$BOOST_DIR.tar.gz
 
+# Make sure the dependencies we grab are in the same branch we are.
+if [ -z "$TRAVIS_BRANCH" ]; then
+    export TRAVIS_BRANCH=`git rev-parse --abbrev-ref HEAD`
+fi
+
 # Make sure we're at the top-level directory when we set up all our siblings.
 cd ..
 
-# Note that we grab shallow clones of the following repos to improve
-# configure/network performance. This may have an affect when making changes
-# within these clones.
+echo_run git_dependency https://github.com/stlab/libraries.git 'libraries'
+echo_run git_dependency https://github.com/stlab/adobe_source_libraries 'adobe_source_libraries'
+echo_run git_dependency https://github.com/stlab/adobe_platform_libraries 'adobe_platform_libraries' master
+echo_run git_dependency https://github.com/stlab/double-conversion.git 'double-conversion' master
 
-# fetch the ASL, APL, and double-conversion libraries.
-if [ ! -e 'double-conversion' ]; then
-    echo "INFO : double-conversion not found: setting up."
-
-    echo_run git clone --branch=master --depth=1 https://github.com/stlab/double-conversion.git
-else
-    echo "INFO : double-conversion found: skipping setup."
-fi
-
-if [ ! -e 'adobe_source_libraries' ]; then
-    echo "INFO : adobe_source_libraries not found: setting up."
-
-    echo_run git clone --branch=master --depth=1 https://github.com/stlab/adobe_source_libraries.git
-else
-    echo "INFO : adobe_source_libraries found: skipping setup."
-fi
-
-if [ ! -e 'adobe_platform_libraries' ]; then
-    echo "INFO : adobe_platform_libraries not found: setting up."
-
-    echo_run git clone --branch=master --depth=1 https://github.com/stlab/adobe_platform_libraries.git
-else
-    echo "INFO : adobe_platform_libraries found: skipping setup."
-fi
+exit 0;
 
 # If need be, download Boost and unzip it, moving it to the appropriate location.
 if [ ! -e 'boost_libraries' ]; then
